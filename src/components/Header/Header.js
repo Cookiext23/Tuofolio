5b93e8008b90919f83bacdccbefbe0ffa6a1ecbd
cookiext23”git config --global user.email cookiext23@gmail.com” <cookiext23@gmail.com>
2024-04-28 23:48:06 +1200

    avatar icon updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
 import { header } from '../../portfolio'
 import Navbar from '../Navbar/Navbar'
 import './Header.css'
+import { ThemeContext } from '../../contexts/theme';
+import image from './avatarIcon.svg'
+import imageDark from './avatarIcon_dark.svg'
 
 const Header = () => {
   const { homepage, title } = header
+  const [{ themeName }] = useContext(ThemeContext);
+  const imageSrc = themeName === 'dark' ? imageDark : image;
 
   return (
     <header className='header center'>
       <h3>
         {homepage ? (
-          <a href={homepage} className='link'>
-            {title}
+          <a href={homepage}className='link link--icon'>
+            <img src={imageSrc} alt={title} className='image' />
           </a>
         ) : (
           title
